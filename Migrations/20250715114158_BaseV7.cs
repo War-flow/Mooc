6b3c6f87e1659cb23453069afd065669283c0d5e
Flow <derka161@gmail.com>
2025-07-15 11:37:53 -0300

    Path TextBlock

---
+++ Migrations/20250715114158_BaseV7.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mooc.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseV7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
